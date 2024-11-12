eb7c37357d44a4e6a05d613500a50731e856299a
alcoffeeocha <alkahfikhuzaimy@gmail.com>
2024-11-12 16:51:44 +0700

    temporarily disable lint after write because nvim-lint has strange error

--- after/plugin/lint.lua
+++ after/plugin/lint.lua
@@ -1,12 +1,22 @@
 local status, lint = pcall(require, "lint")
-if (not status) then return end
+if not status then
+	return
+end
+
 lint.linters_by_ft = {
-	javascript = {'eslint'},
-	typescript = {'eslint'}
+	javascript = { "eslint" },
+	javascriptreact = { "eslint" },
+	typescript = { "eslint" },
+	typescriptreact = { "eslint" },
 }
 
-vim.api.nvim_create_autocmd({"BufWritePost"}, {
+--[[
+local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	group = lint_augroup,
 	callback = function()
 		lint.try_lint()
 	end,
 })
+--]]
